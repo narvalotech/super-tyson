d51daea22641449ddf461204d045bf30cb7dcdc2
Jonathan Rico <jonathan@rico.live>
2024-09-03 15:44:06 +0200

    add hello-world

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ Super-Tyson (ST) is a micro-ide for [micro lisp](http://www.ulisp.com/).
 
 Needed a project to learn me some C++.
 Also ulisp is lacking a small IDE, something like micropython's [Thonny](https://thonny.org/).
+
+# How (to use)
+
+``` sh
+# configure
+cmake -Bbuild -S.
+
+# build
+cmake --build build
+
+# run
+./build/SuperTyson
+```
---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    # C++ compiler (GCC or Clang)
+    pkgs.gcc
+    pkgs.clang
+
+    # CMake
+    pkgs.cmake
+
+    # Other useful tools
+    pkgs.gdb
+    pkgs.valgrind
+    pkgs.clang-tools
+  ];
+
+  # Shell hook to set up environment
+  shellHook = ''
+    echo "Entered C++ development environment"
+  '';
+}
