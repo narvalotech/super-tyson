4ad475cebaf51f9672bda39abeb991271fe2265e
Jonathan Rico <jonathan@rico.live>
2024-09-05 12:37:19 +0200

    build: show how to sniff serial traffic

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ set -eu
 ninja -Cbuild
 
 echo "(+ 1 2 (* 3 4))" | ./build/SuperTyson
+# To sniff traffic:
+# socat -r /dev/fd/0 -R /dev/fd/0 -d0 pty,raw,echo=0,link=/tmp/ttyV0 /dev/ttyACM0,raw,b115200,echo=0
