c5420e98360a64b1bc0361b750f2ca05ecf80f99
Jonathan Rico <jonathan@rico.live>
2024-09-05 14:36:52 +0200

    get it working with a real ulisp device, over serial

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ set -eu
 
 ninja -Cbuild
 
-echo "(+ 1 2 (* 3 4))" | ./build/SuperTyson
 # To sniff traffic:
 # socat -r /dev/fd/0 -R /dev/fd/0 -d0 pty,raw,echo=0,link=/tmp/ttyV0 /dev/ttyACM0,raw,b115200,echo=0
+
+# gdb --args ./build/SuperTyson "/tmp/ttyV0"
+# echo "(+ 1 2 (* 3 4))" | ./build/SuperTyson "/dev/ttyACM0"
+echo "(+ 1 2 (* 3 4))" | ./build/SuperTyson "/tmp/ttyV0"
